c96eb51e46947784654509b3923ae3c8a962ba47
itachig1 <41496765+itachig1@users.noreply.github.com>
2023-03-01 18:59:20 -0300

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,11 @@
+fetch('https://randomuser.me/api/')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+  
+
+  /*const persona = ;
+        const obj = JSON.parse(persona);
+        console.log(obj);*/
+
+
